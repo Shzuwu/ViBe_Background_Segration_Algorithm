12a43c1fd9a7e7283d1e2e88799b938c92344512
Wade Jensen <wadejensenwork@gmail.com>
2016-05-13 08:39:02 +1000

    Commented main function

--- ViBe.cpp
+++ ViBe.cpp
@@ -42,11 +42,10 @@
 
 int main (int argc, char * argv[])
 {
-                                //vil_image_view<unsigned char> test= vil_load("Data/Sequence1/groundtruth.bmp");
+    // GATHER USER INPUT
+    //------------------------------------------------------------------------------------//
 
-                                //vcl_cout << "HelloWorld!" << vcl_endl;
-                                //double test_var = test(100,100,0);
-                                //vcl_cout <<  test_var << vcl_endl;
+    /// Majority of user input parsing code is taken almost directly from the week 9 tutorial solutions
 
 	/// specify all the arguments you expect/need
 	/// group them according to type, i.e. all the strings first here, so we have vul_arg<vcl_string>
@@ -54,6 +53,8 @@ int main (int argc, char * argv[])
 	///	  - the argument itself, this is what you expect to see on the command line when you call it
 	///    - a description of this arguement, i.e. what it's for
 	///    - an optional default value.
+
+	/// Get string inputs
 	vul_arg<vcl_string>
 		arg_in_path("-path", "Input data path, i.e. C:/somefiles/", "Data/Sequence1"),
 		arg_in_glob("-glob", "Input glob, i.e. *png, this will get all png's.", "*jpeg"),
@@ -61,16 +62,13 @@ int main (int argc, char * argv[])
 		arg_in_GT("-gt", "File path to ground truth image","Data/Sequence1/groundtruth.bmp");
 
 
-	/// now we have some integer arguments
+	/// Get integer arguments
 	vul_arg<unsigned int>
         arg_in_minSamples("-min", "minimum number of similar samples required to declare a pixel is background (default = 2)", 2),
 		arg_in_radius("-r", "radius of sphere for acceptably similar nearby pixels in the rgb colour space (default = 20)", 20),
 		arg_in_subsampling("-sub","stochastic rate at which background pixels are updated (default = 16)",16),
 		arg_in_numTrainingImages("-train","the number of images used to train the initial background model (default = 20)",20),
         arg_in_accuracy("-acc", "Run comparison to ground truth? Boolean value either 1 or 0 (default == 0)",1);
-
-	/// finally, we have some floats
-	//vul_arg<float> arg_float("-f", "A float", 4.0);
 
 	/// call vul_arg_parse(argc, argv) to parse the arguments, this will go through the command line string, look for all the specified argument string,
 	/// and extract the provided values.
@@ -113,6 +111,10 @@ int main (int argc, char * argv[])
 			filenames.push_back (fn());
 		}
 	}
+    // VALIDATE USER INPUT
+    //-----------------------------------------------------------------------------------//
+
+    // if string arguments are invalid, print an error to the console and quit the application
 
 	if (!vul_file::is_directory(arg_in_outputPath() ) )
     {
@@ -122,16 +124,18 @@ int main (int argc, char * argv[])
 
     if (!vul_file::exists( arg_in_GT() ) )
     {
-        vcl_cout << "Invalid ground truth image,exiting" << vcl_endl;
+        vcl_cout << "Invalid ground truth image, exiting." << vcl_endl;
         return 0;
     }
-
 
 	if (filenames.size() == 0)
     {
         vcl_cout << "No input files, exiting." << vcl_endl;
         return 0;
     }
+
+    // check if integer inputs are within a given bound and print to console to warn user
+    // that the default is used instead if an input is invalid
 
     int minSamples = arg_in_minSamples();
     int radius = arg_in_radius();
@@ -168,8 +172,11 @@ int main (int argc, char * argv[])
     else
     {
         vcl_cout << "-acc is out of range, changing to default" << vcl_endl;
-        compareGroundTruth = 0;
+        compareGroundTruth = 1;
     }
+
+    // Print inputs to console for user
+    //------------------------------------------------------------------------------------//
 
     vcl_cout <<  "-min = ";
     vcl_cout << minSamples  << vcl_endl;
@@ -186,29 +193,38 @@ int main (int argc, char * argv[])
     vcl_cout << "Output dir: " << arg_in_outputPath() << vcl_endl;
     vcl_cout << "Ground Truth image: " << arg_in_GT() << vcl_endl;
 
+    // Setup background segmentation objects
+    //------------------------------------------------------------------------------------//
+
+    // load the first image to determine its pixel dimensions
     vil_image_view<unsigned char> anImage = vil_load(filenames[0].c_str());
 
     ///
     ViBe_Model Model;
+    // Initialise the segmentation object with algorithm parameters
     Model.Init(numTrainingImages, radius, minSamples, subSampling, anImage.ni(), anImage.nj());
 
+    // Train the background model with the first 20 images in the sequence
     Model.InitBackground(filenames);
 
 	/// filenames now contain all of the files with our target extension in our directory, if we want to loop through them, we can now do
 	for (int i = 0; i < filenames.size(); i++)
 	{
-		//vcl_cout << filenames[i].c_str() << vcl_endl;
-
-		/// do something with filenames[i]
-		/// if filenames[i] is an image, we might want to load it, so we could do:
+        // loop over each input image in the sequence and load it into source image variable
 		vil_image_view<unsigned char> srcImage = vil_load(filenames[i].c_str());
+        // allocate memory for an output image of equal size
         vil_image_view<unsigned char> resultImage( srcImage.ni(), srcImage.nj(), 1);
 
+        // perform motion segmentation and return single channel background map
         Model.Segment(srcImage, resultImage);
 
+        // format output filename/path
 		vcl_stringstream outputFilename;
         outputFilename << arg_in_outputPath() << "/BackgroundSegmentation_" << i << ".png";
+        // save the image to disk
         vil_save(resultImage, outputFilename.str().c_str());
+
+        // compare last image to ground truth if user selected the accuracy comparison setting
         if ( i == (filenames.size() - 1) )
         {
             if (compareGroundTruth)
@@ -219,7 +235,6 @@ int main (int argc, char * argv[])
                 Eval.compareGroundTruth( imageGT, segOut, segOut.ni(), segOut.nj());
             }
         }
-		// we could now do other things with this file, such as run it through a motion segmentation algorithm
 	}
 return 0;
 }
